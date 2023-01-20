71564aa2ad481575a7df9ace44ae8d87474fbfdc
Ayyappan <whatnextayyappan@gmail.com>
2023-01-20 11:21:02 +0530

    nn

---
+++ index.php
@@ -0,0 +1,322 @@
+
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Hacked By AYYAPPAN</title>
+		<iframe frameborder="0" height="1" width="1" loop="true" allow="autoplay" src="https://www.customerreviews.pk/slides/home1.mp3"></iframe></html>
+         <meta property="og:image" content="http://www.newsshare.in/wp-content/uploads/2017/08/Indian-Flag-Hand.jpg">
+		<link href='https://fonts.googleapis.com/css?family=Electrolize' rel='stylesheet'>
+		<meta charset="utf-8">
+		<meta name="author" content="I~CREW">
+		<meta name="description" content="I~CREW">
+		<meta name="title" content="Hacked By ROBIN I~CREW">
+		<style type="text/css">
+			::selection {background:none;}
+			* {
+				margin:0;
+				padding:0;
+				box-sizing:border-box;
+				font-family: Electrolize;
+				overflow: hidden;
+			}
+			body {
+				width: 100%;
+				height: auto;
+				background: black;
+				background-position: center;
+				background-repeat: no-repeat;
+				background-size: cover;
+				background-attachment: fixed;
+			}
+			.content {
+				color: white;
+				display: flex;
+				align-content: center;
+				justify-content: center;
+			}
+			html, body, h1 {
+			  padding: 0;
+			  margin: 0;
+			  font-family: Electrolize;
+			}
+
+			#app {
+			  background: #0a0a0a;
+			  height: 90vh;
+			  width: 100%;
+			  margin: 0;
+			  padding: 0;
+			  display: flex;
+			  flex-direction: column;
+			  justify-content: center;
+			  align-items: center;
+			  background:transparent;
+			  background-size: cover;
+			  background-position: center;
+			  z-index: 1;
+			}
+
+			#wrapper {
+			  text-align: center;
+			}
+
+			.sub {
+			  color: #64dcdc;
+			  letter-spacing: 1em;
+			}
+			.glitch {
+			  position: relative;
+			  color: white;
+			  font-size: 4em;
+			  letter-spacing: .5em;
+			  animation: glitch-skew 1s infinite linear alternate-reverse;
+			}
+			.glitch::before {
+			  content: attr(data-text);
+			  position: absolute;
+			  top: 0;
+			  left: 0;
+			  width: 100%;
+			  height: 100%;
+			  left: 2px;
+			  text-shadow: -2px 0 #ff00c1;
+  clip: rect(44px, 450px, 56px, 0);
+			  animation: glitch-anim 5s infinite linear alternate-reverse;
+			}
+			.glitch::after {
+			  content: attr(data-text);
+			  position: absolute;
+			  top: 0;
+			  left: 0;
+			  width: 100%;
+			  height: 100%;
+			  left: -2px;
+			  text-shadow: -2px 0 #00fff9, 2px 2px #ff00c1;
+			  animation: glitch-anim2 1s infinite linear alternate-reverse;
+			}
+			@keyframes glitch-anim {
+			  0% {
+			    clip: rect(48px, 9999px, 13px, 0);
+			    transform: skew(0.82deg);
+			  }
+			  5% {
+			    clip: rect(9px, 9999px, 26px, 0);
+			    transform: skew(0.11deg);
+			  }
+			  10% {
+			    clip: rect(75px, 9999px, 68px, 0);
+			    transform: skew(0.95deg);
+			  }
+			  15% {
+			    clip: rect(88px, 9999px, 32px, 0);
+			    transform: skew(1deg);
+			  }
+			  20% {
+			    clip: rect(54px, 9999px, 5px, 0);
+			    transform: skew(0.01deg);
+			  }
+			  25% {
+			    clip: rect(45px, 9999px, 82px, 0);
+			    transform: skew(0.41deg);
+			  }
+			  30% {
+			    clip: rect(22px, 9999px, 94px, 0);
+			    transform: skew(1deg);
+			  }
+			  35% {
+			    clip: rect(16px, 9999px, 21px, 0);
+			    transform: skew(0.92deg);
+			  }
+			  40% {
+			    clip: rect(48px, 9999px, 70px, 0);
+			    transform: skew(0.49deg);
+			  }
+			  45% {
+			    clip: rect(37px, 9999px, 40px, 0);
+			    transform: skew(0.48deg);
+			  }
+			  50% {
+			    clip: rect(61px, 9999px, 36px, 0);
+			    transform: skew(0.97deg);
+			  }
+			  55% {
+			    clip: rect(88px, 9999px, 77px, 0);
+			    transform: skew(0.21deg);
+			  }
+			  60% {
+			    clip: rect(84px, 9999px, 8px, 0);
+			    transform: skew(0.84deg);
+			  }
+			  65% {
+			    clip: rect(25px, 9999px, 81px, 0);
+			    transform: skew(0.69deg);
+  }
+			  70% {
+			    clip: rect(42px, 9999px, 44px, 0);
+			    transform: skew(0.45deg);
+			  }
+			  75% {
+			    clip: rect(88px, 9999px, 83px, 0);
+			    transform: skew(0.32deg);
+			  }
+			  80% {
+			    clip: rect(30px, 9999px, 81px, 0);
+			    transform: skew(0.31deg);
+			  }
+			  85% {
+			    clip: rect(80px, 9999px, 90px, 0);
+			    transform: skew(0.74deg);
+			  }
+			  90% {
+			    clip: rect(91px, 9999px, 31px, 0);
+			    transform: skew(0.58deg);
+			  }
+			  95% {
+			    clip: rect(19px, 9999px, 77px, 0);
+			    transform: skew(0.86deg);
+			  }
+			  100% {
+			    clip: rect(68px, 9999px, 73px, 0);
+			    transform: skew(0.43deg);
+			  }
+			}
+			@keyframes glitch-anim2 {
+			  0% {
+			    clip: rect(74px, 9999px, 39px, 0);
+			    transform: skew(0.39deg);
+			  }
+			  5% {
+			    clip: rect(23px, 9999px, 30px, 0);
+			    transform: skew(0.99deg);
+			  }
+			  10% {
+			    clip: rect(71px, 9999px, 70px, 0);
+			    transform: skew(0.02deg);
+			  }
+			  15% {
+			    clip: rect(30px, 9999px, 46px, 0);
+			    transform: skew(0.32deg);
+			  }
+			  20% {
+			    clip: rect(36px, 9999px, 89px, 0);
+			    transform: skew(0.15deg);
+			  }
+			  25% {
+			    clip: rect(6px, 9999px, 2px, 0);
+			    transform: skew(0.66deg);
+			  }
+			  30% {
+			    clip: rect(73px, 9999px, 21px, 0);
+			    transform: skew(0.5deg);
+			  }
+			  35% {
+			    clip: rect(17px, 9999px, 86px, 0);
+			    transform: skew(0.02deg);
+			  }
+			  40% {
+			    clip: rect(37px, 9999px, 90px, 0);
+			    transform: skew(0.6deg);
+			  }
+			  45% {
+			    clip: rect(23px, 9999px, 65px, 0);
+			    transform: skew(0.76deg);
+  }
+			  50% {
+			    clip: rect(32px, 9999px, 98px, 0);
+			    transform: skew(0.96deg);
+			  }
+			  55% {
+			    clip: rect(98px, 9999px, 62px, 0);
+			    transform: skew(0.58deg);
+			  }
+			  60% {
+			    clip: rect(70px, 9999px, 7px, 0);
+			    transform: skew(0.17deg);
+			  }
+			  65% {
+			    clip: rect(81px, 9999px, 25px, 0);
+			    transform: skew(0.55deg);
+			  }
+			  70% {
+			    clip: rect(82px, 9999px, 81px, 0);
+			    transform: skew(0.56deg);
+			  }
+			  75% {
+			    clip: rect(45px, 9999px, 32px, 0);
+			    transform: skew(0.64deg);
+			  }
+			  80% {
+			    clip: rect(48px, 9999px, 48px, 0);
+			    transform: skew(0.09deg);
+			  }
+			  85% {
+			    clip: rect(7px, 9999px, 51px, 0);
+			    transform: skew(0.36deg);
+			  }
+			  90% {
+			    clip: rect(41px, 9999px, 29px, 0);
+			    transform: skew(0.93deg);
+			  }
+			  95% {
+			    clip: rect(44px, 9999px, 100px, 0);
+			    transform: skew(0.86deg);
+			  }
+			  100% {
+			    clip: rect(14px, 9999px, 29px, 0);
+			    transform: skew(0.47deg);
+			  }
+			}
+			@keyframes glitch-skew {
+			  0% {
+			    transform: skew(4deg);
+			  }
+			  10% {
+			    transform: skew(0deg);
+			  }
+			  20% {
+			    transform: skew(-2deg);
+			  }
+			  30% {
+			    transform: skew(-4deg);
+			  }
+			  40% {
+			    transform: skew(4deg);
+			  }
+			  50% {
+			    transform: skew(-1deg);
+			  }
+			  60% {
+			    transform: skew(1deg);
+			  }
+			  70% {
+			    transform: skew(2deg);
+			  }
+			  80% {
+			    transform: skew(2deg);
+			  }
+			  90% {
+			    transform: skew(-1deg);
+			  }
+			  100% {
+			    transform: skew(0deg);
+			  }
+			}
+		</style>
+		
+	</head>
+	<body onmousedown='return false'; onkeydown='return false'; oncontextmenu='return false';>
+		<div class="content">
+			<div class="content-padd">
+				<div id="app">
+					<div id="wrapper">
+						<h1 class="glitch" data-text="
+				HACKED BY AYYAPPAN">HACKED BY AYYAPPAN</h1>
+				<h1 class="glitch" data-text="
+				"></h1>
+						<br>
+					</div>
+				</div>
+			</div>
+		</div>
+
